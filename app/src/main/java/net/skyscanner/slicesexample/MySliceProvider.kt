586a2785e6cff04fd0636ead121b5bfc14a7a793
Guillermo Orellana <guillermo.orellana@skyscanner.net>
2018-06-27 09:44:55 +0100

    live demo yeahhh

--- app/src/main/java/net/skyscanner/slicesexample/MySliceProvider.kt
+++ app/src/main/java/net/skyscanner/slicesexample/MySliceProvider.kt
@@ -1,10 +1,13 @@
 package net.skyscanner.slicesexample
 
+import android.app.PendingIntent
+import android.content.Intent
 import android.net.Uri
 import androidx.core.graphics.drawable.IconCompat
 import androidx.slice.Slice
 import androidx.slice.SliceProvider
 import androidx.slice.builders.ListBuilder
+import androidx.slice.builders.SliceAction
 import androidx.slice.builders.cell
 import androidx.slice.builders.gridRow
 import androidx.slice.builders.header
@@ -18,9 +21,51 @@ class MySliceProvider : SliceProvider() {
         }
         return when (sliceUri.path) {
             "/inspiration" -> createInspirationSlice(sliceUri)
+            "/londroid" -> createLondroidSlice(sliceUri)
             else -> null
         }
     }
+
+    val pi: PendingIntent
+        get() = PendingIntent.getActivity(context, 0, Intent(), 0)
+
+    private fun createLondroidSlice(sliceUri: Uri): Slice? =
+        list(context, sliceUri, ListBuilder.INFINITY) {
+            addAction(
+                SliceAction(
+                    pi,
+                    IconCompat.createWithResource(context, R.drawable.ic_location_on_black_24dp),
+                    "Check Address"
+                )
+            )
+            addAction(
+                SliceAction(
+                    pi,
+                    IconCompat.createWithResource(context, R.drawable.ic_message_black_24dp),
+                    "Message host"
+                )
+            )
+            header {
+                setTitle("Upcoming meetup: Londroid")
+                setSubtitle("Jun 26 - 75 guests")
+            }
+            gridRow {
+                cell {
+                    addImage(IconCompat.createWithResource(context, R.drawable.londroid), ListBuilder.LARGE_IMAGE)
+                }
+            }
+            gridRow {
+                cell {
+                    addTitleText("Check In")
+                    addText("06:00 PM")
+                }
+                cell {
+
+                    addTitleText("Check Out")
+                    addText("09:00 PM")
+                }
+            }
+        }
 
     private fun createInspirationSlice(sliceUri: Uri): Slice? =
         list(context, sliceUri, ListBuilder.INFINITY) {
